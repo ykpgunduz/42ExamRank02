51533c60e7aa2cba9a1f9ea6bae38fff00cc376d
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2025-12-24 10:47:52 +0300

    fix

--- Level_3/ft_atoi_base/ft_atoi_base.c
+++ Level_3/ft_atoi_base/ft_atoi_base.c
@@ -1,19 +1,16 @@
 int	ft_atoi_base(const char *str, int base)
 {
 	char	*number;
-	int		result;
-	int		sign;
-	int		i;
+	int		result = 0;
+	int		sign = 1;
+	int		i = 0;
 
-	i = 0;
-	sign = 1;
 	number = (char *)str;
 	if (number[i] == '-')
 	{
 		sign *= -1;
 		i++;
 	}
-	result = 0;
 	while (number[i] != '\0')
 	{
 		if (number[i] >= '0' && number[i] <= '9')
