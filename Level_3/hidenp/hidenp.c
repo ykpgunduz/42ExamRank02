8c0f84da448eb690aba5d0cd296dfa72c9abe437
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-01-08 19:01:15 +0300

    fix: comment lines deleted

--- Level_3/hidenp/hidenp.c
+++ Level_3/hidenp/hidenp.c
@@ -2,31 +2,23 @@
 
 int main(int ac, char **av)
 {
-    // Sayaçlar: i, s1 için; j, s2 için.
     int i = 0;
     int j = 0;
 
-    // Sadece 2 argüman varsa devam et
     if (ac == 3)
     {
-        // String'lerden herhangi biri bitene kadar devam et
         while (av[1][i] && av[2][j])
         {
-            // Eğer s1'de aradığımız karakteri s2'de bulursak...
             if (av[1][i] == av[2][j])
-                i++; // ...s1'deki bir sonraki karaktere geç.
-            
-            // s2'de her zaman bir sonraki karaktere ilerle.
+                i++;
             j++;
         }
 
-        // Döngü bittiğinde kontrol: s1'in sonuna ulaştık mı?
         if (av[1][i] == '\0')
-            write(1, "1", 1); // Evet, ulaştık.
+            write(1, "1", 1);
         else
-            write(1, "0", 1); // Hayır, ulaşamadık.
+            write(1, "0", 1);
     }
-
     write(1, "\n", 1);
     return (0);
 }
