1a1e3493c7b1dba1bb1a2271a987309869b4954d
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-01-23 01:07:13 +0300

    edited

--- Level_3/ft_range/ft_range.c
+++ Level_3/ft_range/ft_range.c
@@ -1,20 +1,26 @@
 #include <stdlib.h>
 
-int	*ft_range(int start, int end)
+int		*ft_range(int start, int end)
 {
-	int	*result;
-	int		i;
+	int		*result;
+	int		i = 0;
 	int		len;
 
-	i = 0;
-	len = abs(start - end + 1);
-	result = (int *)malloc(len);
+	if (start <= end)
+		len = end - start + 1;
+	else
+		len = start - end + 1;
+
+	result = (int *)malloc(sizeof(int) * len);
+
 	while (i < len)
 	{
-		if (start <= end)
-			result[i++] = start++;
-		else if (start >= end)
-			result[i++] = start--;
+		result[i] = start;
+		if (start < end)
+			start++;
+		else if (start > end)
+			start--;
+		i++;
 	}
-	return(result);
+	return (result);
 }
