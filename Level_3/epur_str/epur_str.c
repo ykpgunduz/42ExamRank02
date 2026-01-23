de23d2e31a6a8fa0415efe3845c655f9d982ac32
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-01-23 10:37:49 +0300

    fixed

--- Level_3/epur_str/epur_str.c
+++ Level_3/epur_str/epur_str.c
@@ -2,31 +2,30 @@
 
 int main(int argc, char **argv)
 {
-	int i;
-	int first_word;
+    int i;
 
-	if (argc == 2)
-	{
-		i = 0;
-		first_word = 1;
-		while (argv[1][i] == ' ' || argv[1][i] == '\t')
-			i++;
-		while (argv[1][i])
-		{
-			if (argv[1][i] == ' ' || argv[1][i] == '\t')
-			{
-				while (argv[1][i] == ' ' || argv[1][i] == '\t')
-					i++;
-				if (argv[1][i])
-					write(1, " ", 1);
-			}
-			else
-			{
-				write(1, &argv[1][i], 1);
-				i++;
-			}
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
+    if (argc == 2)
+    {
+        i = 0;
+        while (argv[1][i] == ' ' || argv[1][i] == '\t')
+            i++;
+        
+        while (argv[1][i])
+        {
+            if (argv[1][i] == ' ' || argv[1][i] == '\t')
+            {
+                while (argv[1][i] == ' ' || argv[1][i] == '\t')
+                    i++;
+                if (argv[1][i])
+                    write(1, " ", 1);
+            }
+            else
+            {
+                write(1, &argv[1][i], 1);
+                i++;
+            }
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
 }
