96c58333beb1b423ac172a59b7589a1b106b990b
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-01-27 16:41:48 +0300

    edited

--- Level_4/sort_int_tab/sort_int_tab.c
+++ Level_4/sort_int_tab/sort_int_tab.c
@@ -1,24 +1,19 @@
-void	swap(int *a, int *b)
+void    sort_int_tab(int *tab, unsigned int size)
 {
-	int	temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-void	sort_int_tab(int *tab, unsigned int size)
-{
-	int	i = 0;
+    unsigned int i = 0;
+    int temp;
 
 	while (i < (size - 1))
-	{
-		if (tab[i] > tab[i + 1])
-		{
-			swap(&tab[i], &tab[i + 1]);
-			i = 0;
-		}
-		else
-			i++;
-	}
+    {
+        if (tab[i] > tab[i + 1])
+        {
+            temp = tab[i];
+            tab[i] = tab[i + 1];
+            tab[i + 1] = temp;
+            
+            i = 0;
+        }
+        else
+            i++;
+    }
 }
