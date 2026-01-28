93e970fe11fd0fc4616e168a243c9c8187fd6b10
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-01-28 19:17:30 +0300

    edited

--- Level_4/fprime/fprime.c
+++ Level_4/fprime/fprime.c
@@ -1,37 +1,31 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void	fprime(int nbr)
+int main(int argc, char **argv)
 {
-	int	divisor;
-	int	first;
-
-	if (nbr == 1)
-	{
-		printf("1");
-		return ;
-	}
-	divisor = 2;
-	first = 1;
-	while (nbr > 1)
-	{
-		if (nbr % divisor == 0)
-		{
-			if (!first)
-				printf("*");
-			printf("%d", divisor);
-			first = 0;
-			nbr /= divisor;
-		}
-		else
-			divisor++;
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-		fprime(atoi(argv[1]));
-	printf("\n");
-	return (0);
+    if (argc == 2)
+    {
+        int nbr = atoi(argv[1]);
+        if (nbr == 1)
+            printf("1");
+        
+        int divisor = 2;
+        int first = 1;
+        
+        while (nbr > 1)
+        {
+            if (nbr % divisor == 0)
+            {
+                if (!first)
+                    printf("*");
+                printf("%d", divisor);
+                first = 0;
+                nbr /= divisor;
+            }
+            else
+                divisor++;
+        }
+    }
+    printf("\n");
+    return (0);
 }
