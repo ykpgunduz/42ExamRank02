b9577c037f7bd6dbac9cd1bb150c4a0c91bb25bc
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-01-30 00:48:25 +0300

    edited

--- Level_3/paramsum/paramsum.c
+++ Level_3/paramsum/paramsum.c
@@ -1,19 +1,19 @@
 #include <unistd.h>
 
-void	ft_putnbr(int n)
+void ft_putnbr(int n)
 {
-	char digit;
+    char c;
 
-	if (n > 9)
-		ft_putnbr(n / 10);
-	digit = (n % 10) + '0';
-	write(1, &digit, 1);
+    if (n >= 10)
+        ft_putnbr(n / 10);
+    c =( n % 10 ) + '0';
+    write(1, &c, 1);
 }
 
-int main(int ac, char **av)
+int main(int ac, char **argv)
 {
-	(void)av;
-	ft_putnbr(ac - 1);
-	write(1, "\n", 1);
-	return (0);
+    (void)argv;
+    ft_putnbr(ac - 1);
+    write(1, "\n", 1);
+    return(0);
 }
