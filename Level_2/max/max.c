c8801b7b53447a2dbe2eda22b31ceea2baf64499
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-01-30 00:39:40 +0300

    edited

--- Level_2/max/max.c
+++ Level_2/max/max.c
@@ -1,15 +1,17 @@
-int	max(int *tab, unsigned int len)
+int		max(int* tab, unsigned int len)
 {
-	int		result;
-	unsigned int	i = 0;
+    unsigned int i = 0;
+    int result;
 
-	result = tab[i];
-	
-	while (i < len)
-	{
-		if (tab[i] > result)
-			result = tab[i];
-		i++;
-	}
-	return (result);
+    if (len == 0 || !tab)
+        return(0);
+
+    result = tab[i];
+    while (i < len)
+    {
+        if (tab[i] > result)
+            result = tab[i];
+        i++;
+    }
+    return(result);
 }
