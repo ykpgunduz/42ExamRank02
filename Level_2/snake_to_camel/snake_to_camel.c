de843a2790a9470a6a7f3d07eda742f684bb21d7
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2026-01-30 09:45:42 +0300

    edited

--- Level_2/snake_to_camel/snake_to_camel.c
+++ Level_2/snake_to_camel/snake_to_camel.c
@@ -1,28 +1,26 @@
 #include <unistd.h>
 
-void	snake_to_camel(char *str)
+int main(int argc, char **argv)
 {
-	char	c;
-	int		i;
+    int i = 0;
+    char temp;
 
-	i = 0;
-	while (str[i++] != '\0')
-	{
-		if (str[i] == '_')
-		{
-			i++;
-			c = str[i] - 32;
-			write(1, &c, 1);
-		}
-		else
-			write(1, &str[i], 1);
-	}
-}
-
-int	main(int ac, char **av)
-{
-	if (ac == 2)
-		snake_to_camel(av[1]);
-	write(1, "\n", 1);
-	return (0);
+    if (argc == 2)
+    {
+        while (argv[1][i])
+        {
+            if (argv[1][i] == '_')
+            {
+                i++;
+                temp = argv[1][i];
+                temp = temp - 32;
+                write(1, &temp, 1);
+            }
+            else
+                write(1, &argv[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+    return(0);
 }
